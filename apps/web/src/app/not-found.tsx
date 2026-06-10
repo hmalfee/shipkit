4f1c613ffc688190b71e81e897faeba21424adcf
HM Alfee <alfee.personal@gmail.com>
2026-06-10 18:18:18 +0600

    Add `not-found` page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { MoveLeft } from 'lucide-react';
+import Link from 'next/link';
+
+import { Button } from '@mento-mark/ui/components/button';
+
+export default function NotFound() {
+    return (
+        <main className="flex min-h-[calc(100vh-4.5rem)] flex-col items-center justify-center gap-4 p-6">
+            <h1 className="text-2xl font-bold">Page does not exist!</h1>
+            <p className="text-muted-foreground">
+                Uh oh! The page you are looking for does not exist.
+            </p>
+            <Link href="/">
+                <Button>
+                    <MoveLeft />
+                    <span>Go to Home</span>
+                </Button>
+            </Link>
+        </main>
+    );
+}
