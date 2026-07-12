130d57ba02b2bafa8246e886dc21b55812fcd357
HM Alfee <alfee.personal@gmail.com>
2026-07-13 00:15:12 +0600

    Drop propagation URL; no third-party API calls

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -10,7 +10,6 @@ import { env } from './env';
 initBrowserTelemetry({
     serviceName: pkg.name,
     otelEndpoint: env.NEXT_PUBLIC_OTEL_PROXY_PATH,
-    propagateToUrls: [new RegExp(env.NEXT_PUBLIC_SERVER_URL)],
     environment: env.NEXT_PUBLIC_ENV,
     extraSpanProcessors: [new DebugIdEnrichingSpanProcessor()],
     extraLogProcessors: [new DebugIdEnrichingLogProcessor()],
