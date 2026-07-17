86c30c6b8d6237fd1ca3287e00944c1e3dc4ca89
HM Alfee <alfee.personal@gmail.com>
2026-07-17 20:36:01 +0600

    chore(sentry): remove sentry package

--- packages/sentry/src/next/index.ts
+++
@@ -1,3 +0,0 @@
-export { withSentryConfig, type SentryConfigOptions } from './config';
-export { initSentryServer, captureRequestError } from './init';
-export { captureRouterTransitionStart } from '@sentry/nextjs';
